23b32455b0a25e87eecbd37ed045cbec70f4d918
Taka <71265675+takakv@users.noreply.github.com>
2025-06-27 22:18:24 +0300

    Enable liking

--- app/root.tsx
+++ app/root.tsx
@@ -19,7 +19,7 @@ export const links: Route.LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0",
+    href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0..1,0",
   },
 ];
 
--- app/routes/posts.$postId.tsx
+++ app/routes/posts.$postId.tsx
@@ -9,19 +9,31 @@ import {
 } from "react-router";
 
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
+  const userId = await requireUserId(request);
+  const userRole = await prisma.user.findUnique({
+    where: { id: userId },
+    select: { role: true },
+  });
+
   const post = await prisma.post.findUnique({
     where: { id: params.postId },
+    include: {
+      _count: {
+        select: { likes: true },
+      },
+      likes: {
+        where: {
+          userId: userId,
+        },
+      },
+    },
   });
 
   if (!post) {
     throw new Error("Postitust ei letiud");
   }
 
-  const userId = await requireUserId(request);
-  const userRole = await prisma.user.findUnique({
-    where: { id: userId },
-    select: { role: true },
-  });
+  console.log(post);
 
   return { role: userRole?.role, post, published: post.published };
 };
@@ -38,6 +50,12 @@ export const action = async ({ request, params }: ActionFunctionArgs) => {
   }
 
   const form = await request.formData();
+  console.log(form);
+
+  const postId = params.postId;
+  if (!postId) {
+    throw new Error("Postitust ei letiud");
+  }
 
   switch (form.get("intent")) {
     case "approve":
@@ -45,16 +63,26 @@ export const action = async ({ request, params }: ActionFunctionArgs) => {
         where: { id: params.postId },
         data: { published: true, approverId: userId },
       });
-      break;
+      return redirect("/posts");
     case "delete":
       await prisma.post.update({
         where: { id: params.postId },
         data: { hidden: true },
       });
+      return redirect("/posts");
+    case "liked":
+      await prisma.postLike.create({
+        data: { postId, userId },
+      });
+      break;
+    case "unliked":
+      await prisma.postLike.delete({
+        where: { postId_userId: { postId, userId } },
+      });
       break;
   }
 
-  return redirect("/posts");
+  return null;
 };
 
 export default function PostRoute() {
@@ -84,6 +112,10 @@ export default function PostRoute() {
   );
 
   const { post } = data;
+  const likeCount = post._count.likes ?? 0;
+  const userLiked = post.likes.length !== 0;
+  const fillState = `'FILL' ${userLiked ? 1 : 0}`;
+
   return (
     <>
       {!data.published ? (
@@ -99,6 +131,20 @@ export default function PostRoute() {
         ) : (
           ""
         )}
+        <div className="flex mt-2">
+          <form method="post">
+            <button
+              name="intent"
+              type="submit"
+              value={userLiked ? "unliked" : "liked"}
+              className="material-symbols-outlined"
+              style={{ fontVariationSettings: fillState }}
+            >
+              favorite
+            </button>
+          </form>
+          <span>{likeCount}</span>
+        </div>
       </article>
       {!data.published && data.role === $Enums.Role.ADMIN ? buttons : ""}
     </>
